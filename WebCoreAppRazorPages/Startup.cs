625e58f3083ffc07473290653257aa5b8529df75
Tomáš Oplt <tomas.oplt@seznam.cz>
2019-11-07 22:29:23 +0100

    log4net

--- WebCoreAppRazorPages/Startup.cs
+++ WebCoreAppRazorPages/Startup.cs
@@ -4,8 +4,10 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using WebCoreApp.EF.Data;
 using WebCoreApp.Infrastructure.Configuration;
+using WebCoreApp.Product.Services;
 
 namespace WebCoreAppRazorPages
 {
@@ -27,11 +29,12 @@ namespace WebCoreAppRazorPages
 
             // Bind the settings instance as a singleton and expose it as an options type (IOptions<SmartSettings>)
             services.Configure<SmartSettings>(Configuration.GetSection("SmartAdmin"));
-            
+            services.AddKendo();
+            services.AddTransient<IProductService, ProductService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
             if (env.IsDevelopment())
             {
@@ -46,7 +49,7 @@ namespace WebCoreAppRazorPages
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
+            loggerFactory.AddLog4Net();
             app.UseRouting();
 
             app.UseAuthorization();
